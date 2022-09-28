56626f3672e2692efe91429e20573d312492b49d
Freedom Loisa <kitakayaloisa@gmail.com>
2022-09-28 15:58:03 +0300

    created organizers model

--- organizers/admin.py
+++ organizers/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Organizer
 
 # Register your models here.
+@admin.register(Organizer)
+class OrganizerAdminView(admin.ModelAdmin):
+
+    model = Organizer
+
+    list_display = (
+        'first_name',
+        'email',
+        'organization',
+        'status',
+    )
--- organizers/models.py
+++ organizers/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Organizer(models.Model):
+
+    SECTOR_CHOICES = (
+        ('University', 'university'),
+        ('Politics', 'politics'),
+        ('Union', 'union'),
+    )
+
+    first_name = models.CharField(max_length=254, blank=False)
+    last_name = models.CharField(max_length=254, blank=False)
+    email = models.EmailField(max_length=254, blank=False)
+    personal_identification = models.CharField(max_length=254, blank=False)
+    phone = models.CharField(max_length=254, blank=False)
+    country = models.CharField(max_length=254, blank=False)
+    organization = models.CharField(max_length=254, blank=False)
+    sector = models.CharField(max_length=30, choices=SECTOR_CHOICES, blank=False)
+    organization_email = models.EmailField(max_length=254, blank=False)
+    organization_phone = models.CharField(max_length=254, blank=False)
+    status = models.BooleanField(default=False, verbose_name="accepted as organizer")
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        ordering = ["-created_date"]
+
+        db_table = "Poll Organizers"
+
+    def __str__(self) -> str:
+        
+        return self.first_name
