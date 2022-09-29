f5b87db19952fac7e48beae1770e6d71f73da9f9
Freedom Loisa <kitakayaloisa@gmail.com>
2022-09-29 08:43:01 +0300

    created candidate model

--- candidates/admin.py
+++ candidates/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Candidate
 
 # Register your models here.
+@admin.register(Candidate)
+class CandidateAdminView(admin.ModelAdmin):
+
+    model = Candidate
+
+    list_display = (
+        'first_name',
+        'email',
+        'organization',
+        'vying_position',
+    )
+
+    list_filter = (
+        'created_date',
+        'updated_date',
+    )
--- candidates/models.py
+++ candidates/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Candidate(models.Model):
+
+    first_name = models.CharField(max_length=254, blank=False)
+    last_name = models.CharField(max_length=254, blank=False)
+    email = models.EmailField(max_length=254, blank=False)
+    personal_identification = models.CharField(max_length=254, blank=False)
+    photo = CloudinaryField('image')
+    country = models.CharField(max_length=254, blank=False)
+    organization = models.CharField(max_length=254, blank=False)
+    vying_position = models.CharField(max_length=254, blank=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        ordering = ["-created_date"]
+
+        db_table = "Poll Candidates"
+
+    def __str__(self) -> str:
+        
+        return self.first_name
