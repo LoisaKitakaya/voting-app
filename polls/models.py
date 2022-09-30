d7497fbcedf80b6091049a3b2d47ee0d4b53e6d2
Freedom Loisa <kitakayaloisa@gmail.com>
2022-09-30 05:30:46 +0300

    created poll model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from organizers.models import Organizer
+from candidates.models import Candidate
 
 # Create your models here.
+class Poll(models.Model):
+
+    seat = models.CharField(max_length=254, blank=False)
+    intro = models.TextField()
+    organizer = models.ForeignKey(Organizer, on_delete=models.CASCADE)
+    candidate = models.ManyToManyField(Candidate)
+    begin_date = models.DateField()
+    end_date = models.DateField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        ordering = ["-created_date"]
+
+        db_table = "Polls Table"
+
+    def __str__(self) -> str:
+        
+        return self.seat
