c32751c6fca107ffdb829610ec19d051dbc5963d
Freedom Loisa <kitakayaloisa@gmail.com>
2022-10-08 06:32:22 +0300

    added report generation feature and changed some deployment settings

--- results/urls.py
+++ results/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('poll_results/<int:id>/', views.poll_results, name="poll-results"),
+    path('poll_report/<int:id>/', views.generate_report, name="generate-report"),
 ]
